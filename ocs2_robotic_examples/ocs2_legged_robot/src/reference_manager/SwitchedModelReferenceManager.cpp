2e6350f164c315d6c3480d2fa6202339334edab5
liqikai <li_qi_kai@qq.com>
2023-04-15 13:34:16 +0800

    fix tcp and footheight

--- ocs2_robotic_examples/ocs2_legged_robot/src/reference_manager/SwitchedModelReferenceManager.cpp
+++ ocs2_robotic_examples/ocs2_legged_robot/src/reference_manager/SwitchedModelReferenceManager.cpp
@@ -56,7 +56,7 @@ void SwitchedModelReferenceManager::modifyReferences(scalar_t initTime, scalar_t
   const auto timeHorizon = finalTime - initTime;
   modeSchedule = gaitSchedulePtr_->getModeSchedule(initTime - timeHorizon, finalTime + timeHorizon);
 
-  const scalar_t terrainHeight = 0.0;
+  const scalar_t terrainHeight = 0.02;
   swingTrajectoryPtr_->update(modeSchedule, terrainHeight);
 }
 
